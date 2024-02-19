0f6d31551b3d0e7c57a6b8a5d34be7a032936ae6
Joseph Sanchirico <145707725+jsanch93@users.noreply.github.com>
2024-02-19 21:01:20 +0000

    added js added svgs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,47 @@
 console.log('OK');
+
+
+
+gsap.to( '#big-heart', {
+    scale : 2,
+    duration: 2,
+    repeat: -1,
+    yoyo: true
+  });
+  
+  
+  // gsap.to('.ball', {
+  //   scale: 2,
+  //   yoyo: true,
+  //   duration:2.5,
+  //   ease: "steps(12)",
+  //   y: -500
+  // });
+  
+  
+  gsap.from('#bowl', {
+    x: 1000,
+    ease: 'bounce',
+    duration: 3,
+    delay: 4
+    
+  })
+  
+  gsap.from('#handle', {
+    y: -550,
+    ease: 'bounce',
+    delay: 3,
+    duration: 2,
+    rotate: 360
+  })
+  
+  gsap.from('.letter', {
+    
+    y: 1000, 
+    ease: 'elastic',
+    
+    stagger: {
+      each: 1,
+      from: 'edges'
+    }
+  });
